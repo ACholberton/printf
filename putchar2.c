eb52c4e5e02a5e9f6169381fcb12c0170ccbcd3e
ACholberton <1103@holbertonschool.com>
2019-10-26 20:34:58 +0000

    deleted changes

--- putchar2.c
+++ putchar2.c
@@ -1,19 +1,6 @@
 #include <unistd.h>
 
-int _strlen(char *s)
+int putchar2(char c)
 {
-	int i = 0;
-	while (*s)
-	{
-		i++;
-		s++;
-	}
-	return (i);
-}
-
-int putchar2(char c, char *s)
-{
-	int i = 0;
-	i = _strlen(s);
-	return (write(1, &c, i));
+	return (write(1, &c, 1));
 }
