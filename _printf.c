18bb9cf4dab7fc87d7c6d4481eec1492ebe1d92a
ACholberton <1103@holbertonschool.com>
2019-10-26 23:28:46 +0000

    added arguments using variadic functions

--- _printf.c
+++ _printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "holberton.h"
+#include <stdarg.h>
 /**
  *_printf - function will produce output according to format
  *@format: will be the imput entered into the function
@@ -8,13 +9,27 @@
  */
 int _printf(const char *format, ...)
 {
-	if (format == NULL)
+	int a = 0;
+	va_list argument;
+
+	va_start(argument, format);
+
+	while (format[a])
 	{
-		return;
+		switch (format[a])
+		{
+		case 's': s = va_arg(argument, char *);
+			putchar2('s');
+			break;
+		case 'c': c = va_arg(argument, char);
+			putchar2('c');
+			break;
+		case '%': % = va_arg(argument, double);
+			putchar2('%');
+			break;
+		}
+		a++;
 	}
-	else
-	{
-		printf("%s/n", format);
-	}
-	return (0);
+	va_end(argument);
+	return (a);
 }
