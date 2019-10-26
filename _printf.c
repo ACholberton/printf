7ce8928825e083827cb098d035989b4330bcefd6
ACholberton <1103@holbertonschool.com>
2019-10-26 18:50:00 +0000

    created function prototype that will be updated in future

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ *_printf - function will produce output according to format
+ *@format: will be the imput entered into the function
+ *Return: the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	return (0);
+}
