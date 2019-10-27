7fc370cbe7f4a8684181f8ee05ce6dbbefe294fc
christinepang1 <christinezpang@gmail.com>
2019-10-27 18:47:29 +0000

    Adding file

--- _printf.c
+++ _printf.c
@@ -1,41 +1,16 @@
 #include "holberton.h"
-/**
- *_strlen - this function returns the lenght of  string
- *@s: is a string
- *Return: lenght
- */
-int _strlen(char *s)
-{
-	int l;
-
-	l = 0;
-
-	while (*s)
-	{
-		s++;
-		l++;
-	}
-	return (l);
-}
 #include <stdio.h>
 #include <stdlib.h>
-#include "holberton.h"
 /**
  *_printf - function will produce output according to format
  *@format: will be the imput entered into the function
  *Return: the number of characters printed
  */
-
-int print_string(va_list)
-{
-	char str;
-	str = va_arg(ap, char *);
-        putchar2(str);
-}
-
-
 int _printf(const char *format, ...)
 {
-	Printf("Hello there!");
-	return (0);
+	int i = 0;
+
+	if (format != %)
+		putchar2(format[i++]);
+	if (format == %)
 }
