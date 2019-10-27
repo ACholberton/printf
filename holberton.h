fa5225a312c1b34b89e5752e1dc50b74c6f6fadb
ACholberton <1103@holbertonschool.com>
2019-10-27 19:39:16 +0000

    switched the order of the prototypes

--- holberton.h
+++ holberton.h
@@ -5,8 +5,8 @@ int putchar2(char c);
 int _printf(const char *format, ...);
 int _strlen(char *s);
 
-int print_string(va_list);
 int print_char(va_list);
+int print_string(va_list);
 int print_percent(va_list);
 
 typedef struct options
