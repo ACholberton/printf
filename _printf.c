c0bc8af6a9974373b148376e303d76dbaf4af788
ACholberton <1103@holbertonschool.com>
2019-10-30 01:23:01 +0000

    removed comments

--- _printf.c
+++ _printf.c
@@ -30,8 +30,8 @@ int _printf(const char *format, ...)
 	}
 
 	va_start(arg, format);
-/* DO NOT DELETE THIS I = 0; IT IS VERY IMPORTANT */
 	i = 0;
+
 	while (format != NULL && format[i] != '\0')
 	{
 		if (format[i] == '%' && format[i + 1] != '%')
@@ -65,7 +65,6 @@ int _printf(const char *format, ...)
 		{
 			putchar2(format[i]);
 			printedchar += 1;
-			/*		putchar2('*'); */
 		}
 		i++;
 	}
